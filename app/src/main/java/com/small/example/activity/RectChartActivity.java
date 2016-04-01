1224baa2d89358e01bc5f49d48fe6d1baef87226
small.mr <cao_cgq@163.com>
2016-04-01 19:31:31 +0800

    modify

--- app/src/main/java/com/small/example/activity/RectChartActivity.java
+++ app/src/main/java/com/small/example/activity/RectChartActivity.java
@@ -2,6 +2,7 @@ package com.small.example.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.small.example.R;
@@ -10,6 +11,7 @@ import com.small.example.view.RectChart;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by cao on 2016/4/1.
@@ -17,21 +19,32 @@ import java.util.List;
 public class RectChartActivity extends Activity {
 
     private LinearLayout mChartLL;
-
+    private RectChart mRc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_rect_chart);
-
-        mChartLL = (LinearLayout) super.findViewById(R.id.ll_chart);
+        mRc = (RectChart) super.findViewById(R.id.rc_view);
         List<RectData> list = new ArrayList<>();
-        for(int i = 0;i<5000;i++){
+        for(int i = 0;i<5;i++){
+            Random random = new Random();
+            int count = random.nextInt(9999);
             RectData rcs = new RectData();
-            rcs.height = i+10;
-
+            rcs.height = i+1;
             list.add(rcs);
         }
+        mRc.setData(list);
+    }
 
-        mChartLL.addView(new RectChart(this,list));
+    public void setWidth(){
+
+    }
+
+    public void scroll(View view ){
+        mRc.scroll();
+    }
+
+    public void scroll1(View view){
+        mRc.scroll1();
     }
 }
--- app/src/main/java/com/small/example/view/RectChart.java
+++ app/src/main/java/com/small/example/view/RectChart.java
@@ -5,9 +5,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Scroller;
 
 import com.small.example.model.RectData;
 
@@ -18,35 +20,46 @@ import java.util.List;
  * Created by cao on 2016/4/1.
  */
 public class RectChart extends View {
-    private Context context ;
-
     private List<RectData> datas;
     private List<PointF> points;
+
+    private Scroller mScroller;
 
     private float spacingOfX; // X间距
     private Paint rectPaint;//矩形画笔
     private float maxHeight;
-    public RectChart(Context context,List<RectData> list) {
+    private int clickX=0;
+    private int moveX=0;
+    public RectChart(Context context) {
         super(context);
-        this.context = context;
-        this.datas = list;
+        init(context);
+    }
+
+    public RectChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public RectChart(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         init(context);
     }
 
     private void init(Context context){
-        setLayoutParams(new ViewGroup.LayoutParams(5000, ViewGroup.LayoutParams.MATCH_PARENT));
+        mScroller = new Scroller(context);//平滑滚动的scroller 用于知道自定义view使用
+
         rectPaint = new Paint();
         rectPaint.setColor(Color.parseColor("#3A5FCD"));
-        rectPaint.setStrokeWidth(10);
+        rectPaint.setStrokeWidth(60);
     }
-
-
 
     public void setData(List<RectData> list){
         if(list!= null){
             this.datas = list;
             maxHeight = findMaxPowers(datas);
             initPoints();
+//            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(list.size() * 60,600);
+//            setLayoutParams(lp);
 
             invalidate();
         }
@@ -81,16 +94,15 @@ public class RectChart extends View {
         for(int i = 0;i<datas.size();i++){
             float f = datas.get(i).height;
             float y = f/(maxHeight/this.getHeight());
-            spacingOfX = 20;//(float) (this.getWidth() - getPaddingLeft() - getPaddingRight()) / (float) (datas.size() - 1);
+            spacingOfX = paint.getStrokeWidth()+20;//(float) (this.getWidth() - getPaddingLeft() - getPaddingRight()) / (float) (datas.size() - 1);
             float maxX = getWidth()/2;
-            float x = maxX - (i * spacingOfX + this.getPaddingLeft());
+            float x =  (i * spacingOfX)+moveX;
             canvas.drawLine(x, this.getHeight(), x, y, paint);
         }
     }
 
     /**
      * 找到 数据集合中最大值
-     *
      * @return
      */
     private static float findMaxPowers(List<RectData> energys) {
@@ -104,4 +116,53 @@ public class RectChart extends View {
         return energy.height;
     }
 
+    private int lastX = 0;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int)event.getX();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                clickX = x;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveX = lastX+(x-clickX);
+                Log.e("cgq",""+moveX+"==="+(x-clickX));
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                lastX = moveX;
+                scroll((int)(getX()+lastX),lastX);
+                break;
+        }
+        return true;
+    }
+
+    private void scroll(int startX,int x){
+        mScroller.startScroll(-startX, 0, -x, 0,500);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        //先判断mScroller滚动是否完成
+        if (mScroller.computeScrollOffset()) {
+            //这里调用View的scrollTo()完成实际的滚动
+
+            scrollTo(mScroller.getCurrX(), 0);
+//            必须调用该方法，否则不一定能看到滚动效果
+            postInvalidate();
+        }
+        Log.e("cgq", "" + getX());
+        super.computeScroll();
+    }
+
+    public void scroll(){
+        mScroller.startScroll(-100, 0, -200, 0, 5000);
+        invalidate();
+    }
+
+    public void scroll1(){
+        mScroller.startScroll(-300, 0, -400, 0,5000);
+        invalidate();
+    }
 }
